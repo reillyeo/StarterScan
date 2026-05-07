9bb65a432f46c2db6f583aad618f66c9c2655558
Eoghan <reillyeo@tcd.ie>
2026-05-07 13:46:06 +0200

    setting global cran mirror in r script

--- merge_results.R
+++ merge_results.R
@@ -1,3 +1,5 @@
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
 # Install packages if needed
 if (!require("dplyr")) install.packages("dplyr")
 if (!require("readr")) install.packages("readr")
@@ -10,18 +12,21 @@ suppressMessages(library(dplyr))
 
 # Option sorting
 option_list = list(
-  make_option(c("-o", "--output_dir"), type = "character",
-              help = "Path to the output directory")                      
+    make_option(c("-o", "--output_dir"), type = "character",
+            help = "Path to the output directory"),
+    make_option(c("-g", "--gene_info"), type = "character",
+            help = "Path to gene-product info file")                      
 )
 
 # Parse the command line arguments
 args <- parse_args(OptionParser(option_list = option_list))
 
 output_dir <- args$output_dir
+gene_info <- args$gene_info
 
 # Load data
 count_data <- read_tsv(paste(output_dir,"/gene_detection_results.tsv",sep=""))
-annotation_data <- read_delim("~/my_scripts/starterscan/data/gene_products.csv", delim=";", col_names = c("gene","product"))
+annotation_data <- read_delim(gene_info, delim=";", col_names = c("gene","product"))
 
 merged_data <- count_data %>% left_join(annotation_data, by = "gene",unmatched = "drop") %>%
     select(c(product,read_count,RP2Mb)) %>%
